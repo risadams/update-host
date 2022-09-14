5d86d54d025230c0e7c273cb2d17b45d488a787b
Ris Adams <chris@tuesdaybegins.com>
2022-09-14 09:20:36 -0400

    Updated script

--- update-host.sh
+++ update-host.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-#take the first argument as a file name
-FILE=$1
-#read the file line by line
-while read LINE; do
+# take a filename as an argument
+# the file contains urls on each line
+# foreach url resolve the ip and update the host file
 
-  for url in $LINE; do # ensure each argument is a valid url
-    if [[ $url =~ ^https?://[a-zA-Z0-9./-]+$ ]]; then
-      # get the host name from the url
-      host=$(echo $url | awk -F/ '{print $3}')
-      # get the ipv4 address of the host
-      ip=$(dig +short $host)
-      #foreach entry in ip echo the host and ip
-      for i in $ip; do
-
-        #if i is an ipv4 address
-        if [[ $i =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-          # update the host file
-          echo "$ip $host" #>>/etc/hosts
-        else
-          echo "Invalid ip: $ip - $host"
-        fi
-      done
-    else
-      echo "Invalid url: $url"
-    fi
-  done
-
-done <$FILE
+filename=$1
+while read -r line; do
+    ip=$(dig +short $line | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1)
+    echo "$ip $hostname" >> /etc/hosts
+done <$filename
